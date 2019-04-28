cc8a5bdaf12042188973a543df9462d47bb316b4
primetimer <primetimertime@gmail.com>
2019-04-28 09:19:11 +0200

    add target

---
+++ UlamDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UlamDemo
+//
+//  Created by Stephan Jancar on 28.04.19.
+//  Copyright © 2019 Ulam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
